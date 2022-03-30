2c7cff6bfefa1fe7ab2e0a281972626d236424fc
VladislavS999 <phpmailer1999@gmail.com>
2022-03-30 12:12:45 +0300

    Создана заготовка эндпоинта заявок

---
+++ database/migrations/2022_03_30_090525_create_bids_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('category')->nullable();
+            $table->date('date')->nullable();
+            $table->text('message')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
